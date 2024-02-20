dbdedb9c7d46b0515a46fd13fdc366e052371dd4
Alexander Frick <Alex313031@gmail.com>
2024-02-20 04:07:37 -0600

    add sse4 win

--- init_apm_packages.sh
+++ init_apm_packages.sh
@@ -26,6 +26,7 @@ displayHelp () {
 	printf "${bold}${YEL}Use the --win flag for Windows (in bash)${c0}\n" &&
 	printf "${bold}${YEL}Use the --mac flag for MacOS (in bash)${c0}\n" &&
 	printf "${bold}${YEL}Use the --sse4 flag for Linux (SSE4 Version).${c0}\n" &&
+	printf "${bold}${YEL}Use the --win-sse4 flag for Linux (SSE4 Version).${c0}\n" &&
 	printf "${bold}${YEL}Use the --help flag to show this help${c0}\n" &&
 	printf "\n"
 }
@@ -174,6 +175,53 @@ case $1 in
 	--win) initWin; exit 0;;
 esac
 
+initWinSSE4 () {
+# Optimization parameters
+export CFLAGS="-DNDEBUG -msse3 -mssse3 -msse4.1 -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough -Wno-cast-function-type" &&
+export CXXFLAGS="-DNDEBUG -msse3 -mssse3 -msse4.1 -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough -Wno-cast-function-type" &&
+export CPPFLAGS="-DNDEBUG -msse3 -mssse3 -msse4.1 -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough -Wno-cast-function-type" &&
+export LDFLAGS="-Wl,-O3 -msse3 -mssse3 -msse4.1 -s" &&
+export VERBOSE=1 &&
+export V=1 &&
+
+# Set msvs_version for node-gyp on Windows
+export MSVS_VERSION="2019" &&
+export GYP_MSVS_VERSION="2019" &&
+# Download electron binaries here
+export ELECTRON_CACHE="${PWD}/electron/bin" &&
+export electron_config_cache="${PWD}/electron/bin" &&
+
+printf "\n" &&
+printf "${bold}${GRE} Bootstrapping \`dot-atom/packages\` with \`npm install\`...${c0}\n" &&
+printf "\n" &&
+
+# Workaround for git:// URLs
+if [ -e ~/.gitconfig ]
+  then
+  cp -i -v ~/.gitconfig ~/.gitconfig_bak
+fi
+cp -v ./gitconfig ~/.gitconfig &&
+printf "\n" &&
+
+cd ./packages/atom-material-ui &&
+npm run build && cd ..&&
+cd atom-material-syntax-dark &&
+npm run build && cd .. && cd .. &&
+
+cd ./dot-atom/packages &&
+cd atom-ng-browser &&
+npm run build && cd .. &&
+cd color-picker &&
+npm run build && cd .. &&
+cd minimap && npm run build &&
+cd .. && cd .. && cd .. &&
+
+printf "\n"
+}
+case $1 in
+	--win-sse4) initWinSSE4; exit 0;;
+esac
+
 initMac () {
 # Optimization parameters
 export CFLAGS="-DNDEBUG -msse3 -mssse3 -msse4.1 -msse4.2 -mavx -maes -O3 -Wno-deprecated-declarations -Wno-implicit-fallthrough -Wno-cast-function-type" &&
@@ -226,6 +274,7 @@ printf "${bold}${YEL}Use the --linux flag for Linux.${c0}\n" &&
 printf "${bold}${YEL}Use the --win flag for Windows (in bash)${c0}\n" &&
 printf "${bold}${YEL}Use the --mac flag for MacOS (in bash)${c0}\n" &&
 printf "${bold}${YEL}Use the --sse4 flag for Linux (SSE4 Version).${c0}\n" &&
+printf "${bold}${YEL}Use the --win-sse4 flag for Linux (SSE4 Version).${c0}\n" &&
 printf "${bold}${YEL}Use the --help flag to show this help${c0}\n" &&
 printf "\n" &&
 tput sgr0
